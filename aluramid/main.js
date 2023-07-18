1dc5460508b7d6ab60d6d537865a741e8a558da4
Azeve-do <caiopaznunes@gmail.com>
2023-07-17 21:33:46 -0300

    clone

---
+++ aluramid/main.js
@@ -0,0 +1,24 @@
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla')
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+
+    const instrumento = tecla.classList[1];
+
+    console.log(instrumento)
+
+    tecla.onclick = function () {
+        tocaSom('#som_tecla_pom')
+    };
+
+    
+    contador = contador + 1;
+    console.log(contador);
+}
