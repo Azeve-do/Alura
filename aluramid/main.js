0d6d0533014edef4ee47c9e7401977d1fabbc8d7
Azeve-do <caiopaznunes@gmail.com>
2023-07-18 20:49:23 -0300

    código otimizado

--- aluramid/main.js
+++ aluramid/main.js
@@ -4,21 +4,13 @@ function tocaSom(idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla')
 
-let contador = 0;
-
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
-
     const instrumento = tecla.classList[1];
-
-    console.log(instrumento)
-
-    tecla.onclick = function () {
-        tocaSom('#som_tecla_pom')
-    };
-
+    const idAudio = `#som_${instrumento}`
     
-    contador = contador + 1;
-    console.log(contador);
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    };
 }
