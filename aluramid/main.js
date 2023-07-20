62632748cbacebb5b52781300802ea23dc466f9a
Caio <122945848+Azeve-do@users.noreply.github.com>
2023-07-20 14:16:52 -0300

    finalizado alura mid

--- aluramid/main.js
+++ aluramid/main.js
@@ -1,5 +1,15 @@
-function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+   const elemento = document.querySelector(seletorAudio);
+    if (elemento === null) {
+    }
+    
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();  
+    }
+    else {
+        alert('elemento foi pro espaço');
+        }
+        
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla')
@@ -9,13 +19,18 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`
-    
+
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
 
-    tecla.onkeydown = function () {
-        tecla.classList.add('ativa')
+    tecla.onkeydown = function (e) {
+        if (e.code === "Enter" || e.code === "Space") {
+            tecla.classList.add('ativa')
+        }
+        else {
+            tecla.classList.remove('ativa')
+        }
     }
 
     tecla.onkeyup = function() {
