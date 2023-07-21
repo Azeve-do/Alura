9322af5925d1a7dcb6d7f600bcfd50685e079e80
Caio <122945848+Azeve-do@users.noreply.github.com>
2023-07-21 12:55:32 -0300

    otimizado exe de alterar P

---
+++ excercises/7DAYS_OF_CODE/Dia_01/js.js
@@ -0,0 +1,18 @@
+let numeroUm = 1
+let stringUm = '1'
+let numeroTrinta = 30
+let stringTrinta = '30'
+let numeroDez = 10
+let stringDez = '10'
+
+if (numeroUm !== stringUm &&  numeroUm == stringUm) {
+    console.log('Mesmo valor, tipos diferentes')
+} else {
+    console.log('As variáveis não têm o mesmo valor')
+}
+
+if (numeroDez !== stringDez && numeroDez == stringDez) {
+    console.log('Mesmo valor, tipos diferentes')
+    } else {
+        console.log('n tem o mesmo valor')
+}
