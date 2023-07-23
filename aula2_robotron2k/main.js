aef82c21d0c142203ce5208ab11858720dd5996f
Azeve-do <caiopaznunes@gmail.com>
2023-07-22 21:29:37 -0300

    JS Robotron

---
+++ aula2_robotron2k/main.js
@@ -0,0 +1,12 @@
+const Robotron = document.querySelector('#Robotron')
+
+Robotron.addEventListener('click', function() {
+    console.log('Cliquei no robo')
+})
+
+function dizOi() {
+  console.log("oi")
+  console.log('Bem vindo fdp')
+} 
+
+dizOi()
