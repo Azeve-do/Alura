342a4bbb8f37a05a4c12dbc54e856a754fa7c087
Caio <122945848+Azeve-do@users.noreply.github.com>
2023-07-24 17:20:56 -0300

    inicio challenger day 03

---
+++ excercises/7DAYS_OF_CODE/Dia_03_jogoDeEscolhas/js.js
@@ -0,0 +1,14 @@
+
+
+const respostas = document.querySelector('input[type=text]')
+const respostasvalor = respostas.value
+
+if (respostasvalor == 'Front-End') {
+    console.log('KKK')
+    event.preventDefault();
+}
+
+for (let i = 0; i < 0; i++) {
+    
+
+}
