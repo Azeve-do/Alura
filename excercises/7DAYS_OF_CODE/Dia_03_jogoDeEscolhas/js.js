8148ec7cd6348617f6fbe809c5015bcc03c535fa
Caio <122945848+Azeve-do@users.noreply.github.com>
2023-07-25 16:07:25 -0300

    challenge dia 03 concluído

--- excercises/7DAYS_OF_CODE/Dia_03_jogoDeEscolhas/js.js
+++ excercises/7DAYS_OF_CODE/Dia_03_jogoDeEscolhas/js.js
@@ -1,14 +1,30 @@
+const modalidade = prompt('Front-End  ou Back-End ?')
+let linguagem = '';
 
-
-const respostas = document.querySelector('input[type=text]')
-const respostasvalor = respostas.value
-
-if (respostasvalor == 'Front-End') {
-    console.log('KKK')
-    event.preventDefault();
+if (modalidade === 'Front-End') {
+    linguagem = prompt('Você quer aprender React ou Vue?')
+} else if (modalidade === 'Back-End') {
+    linguagem = prompt('Você quer aprender C# ou Java?')
+} else {
+    alert('resposta inválida')
 }
 
-for (let i = 0; i < 0; i++) {
-    
+const fullstackOuEspecializacao = prompt('Você quer se espeacilizar na linguagem (1) ou seguir full stack (2)?')
 
+if(fullstackOuEspecializacao == 1) {
+    alert(`continue aprendendo ${linguagem} para ser espeacilizar em ${modalidade}`)
+} else if (fullstackOuEspecializacao == 2) {
+    alert(`Se esforce bastante!`)
+}
+
+let maislinguagem = prompt('Gostaria de aprender mais alguma linguagem? ')
+
+var upperMaisLinguagem = maislinguagem.toUpperCase()
+
+while (upperMaisLinguagem === 'SIM') {
+    let novaLinguagem = prompt('Qual?')
+    maislinguagem = prompt('Gostaria de aprender mais alguma linguagem? ')
+    if (maislinguagem !== 'SIM') {
+        break
+    }
 }
