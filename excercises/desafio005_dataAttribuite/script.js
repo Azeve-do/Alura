95309837e795f6abbc191f7ac0daaed2f49ce2b1
Azeve-do <caiopaznunes@gmail.com>
2023-07-25 20:45:58 -0300

    robotron e desafio005 finalizados

---
+++ excercises/desafio005_dataAttribuite/script.js
@@ -0,0 +1,9 @@
+const listaCores = document.querySelector(".lista")
+
+function apagaTinta() {
+    listaCores.style.display = 'none'
+}
+
+function mostraTintas() {
+    listaCores.style.display = 'block'
+}
