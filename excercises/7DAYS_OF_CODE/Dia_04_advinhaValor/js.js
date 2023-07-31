ba2d435ca260d4be58db649cebc939d79687ab02
Caio <122945848+Azeve-do@users.noreply.github.com>
2023-07-31 17:15:39 -0300

    atividade do dia 04 e 05

--- excercises/7DAYS_OF_CODE/Dia_04_advinhaValor/js.js
+++ excercises/7DAYS_OF_CODE/Dia_04_advinhaValor/js.js
@@ -1,41 +1,25 @@
-var gerador = Math.floor(Math.random() * (0, 10))
+var gerador = Math.floor(Math.random() * (1, 11))
 const formulario = document.querySelector('#meuForm')
-
 var tentativas = 3
-
-console.log(gerador)
 
 formulario.addEventListener('submit', (event) => {
     event.preventDefault();
     
     var resposta = document.querySelector('#resposta').value;
     
-    
     if(resposta == gerador) {
         alert('Parabéns você acertou! Jogo resetado.')
         location.reload();
     } 
     
-    else if(tentativas > 0) {
+    else if(tentativas > 1) {
          tentativas = tentativas - 1    
          alert('Resposta errada, tente novamente,' + ` mais ${tentativas} tentativas!`)  
         }
            
-    else if(tentativas == 0) {  
+    else if(tentativas == 1) {  
         alert('tentativas encerradas, jogo resetado!' + ` Número sorteado era ${gerador}`)
         location.reload();
 }
-    console.log(tentativas)
-
 }
-
-
 )
-
-/* 
-    if(resposta != gerador) {
-        alert('Resposta errada, tente novamente' + ` estava pensando no ${gerador}` + ` e você respondeu ${resposta}`)
-    } else if (resposta == gerador) {
-        alert('Parabéns você acertou')
-        location.reload();
-    }  */
---
+++ excercises/7DAYS_OF_CODE/Dia_05_manuseioListas/js.js
@@ -0,0 +1,31 @@
+var frutas = []
+var açougue = []
+var mercearia = []
+
+var adicionarItem = prompt('LISTA DE COMPRAS, CONTINUAR ? Sim ou Não.')
+
+while (adicionarItem == 'Sim') {
+    var adicionarItem = prompt('Quer adicionar algum item na lista? Sim ou Não.')
+
+    if (adicionarItem == 'Sim') {  
+    var item = prompt('Qual item?')
+    var categoria = prompt('Em qual categoria?')
+
+    if (categoria == 'frutas') {
+        frutas.push(item)
+    } 
+    
+    else if (categoria == 'açougue') {
+        açougue.push(item)
+    } 
+
+    else if (categoria == 'mercearia') {
+        mercearia.push(item)
+    }
+}
+    if (adicionarItem == 'Não') {
+        alert('Lista finalizada')
+        console.log('lista ficou assim')
+        console.log(`Frutas: ${frutas}. Açougue: ${açougue}. Mercearia: ${mercearia}`)
+    } 
+}
