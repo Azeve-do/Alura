b552e9fc201e90ea8bbf66ef2a69a1d2d4be2234
Azeve-do <caiopaznunes@gmail.com>
2023-08-01 21:39:23 -0300

    refatorado mochilaListas

--- aula2_robotron2k/main.js
+++ aula2_robotron2k/main.js
@@ -37,15 +37,6 @@ const pecas1 = {
 const imgTrocar = document.getElementById('Robotron')
 const trocacor = document.getElementById('trocacor')
 
-/* function trocarImagem () {
-  if(imgTrocar.src.endsWith('img/robotronblack.png')) {
-    imgTrocar.src = "img/robotronyellow/robotronamarelo.png"
-  } else {
-    imgTrocar.src = "img/robotronblue/robotronazul.png"
-  }
-} */
-
-
 trocacor.addEventListener('click', () => {
 if(imgTrocar.src.endsWith('img/robotronblack.png')) {
   imgTrocar.src = 'img/robotronyellow/robotronamarelo.png'
@@ -54,7 +45,6 @@ if(imgTrocar.src.endsWith('img/robotronblack.png')) {
 }
 
 })
-
 
 controle.forEach( (elemento) => {
   elemento.addEventListener('click', (evento) => {
