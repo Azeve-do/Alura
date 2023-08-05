cd081808c15bdb6ba3595964512bc2ed60b3f496
Azeve-do <caiopaznunes@gmail.com>
2023-08-05 17:50:21 -0300

    day 6 completed

--- excercises/7DAYS_OF_CODE/Dia_06_manuseioListas(REMOVE)/js.js
+++ excercises/7DAYS_OF_CODE/Dia_06_manuseioListas(REMOVE)/js.js
@@ -7,25 +7,11 @@ var mercearia = []
 var adicionarItem = prompt('LISTA DE COMPRAS, CONTINUAR ? Sim ou Não.')
 
 while (adicionarItem == 'Sim') {
-    var adicionarItem = prompt('Quer adicionar algum item na lista? Sim ou Não.')
+    var adicionarItem = prompt('Quer adicionar algum item na lista? Sim, Não ou Remover?')
     atribuindoValor()
 
-    if (adicionarItem == 'Não') {
-
-        alert('Lista finalizada')
-                    
-        var categoriaFrutas = document.createElement('p')
-        categoriaFrutas.innerHTML = 'Frutas: ' + frutas
-
-        var categoriaAçougue = document.createElement('p')
-        categoriaAçougue.innerHTML = 'Açougue: ' + açougue
-
-        var categoriaMercearia = document.createElement('p')
-        categoriaMercearia.innerHTML = 'Mercearia: ' + mercearia
-
-        lista.appendChild(categoriaFrutas)
-        lista.appendChild(categoriaAçougue)
-        lista.appendChild(categoriaMercearia)
+    if (adicionarItem == 'Remover') {
+        removerValor()
     }
 }
 
@@ -46,5 +32,58 @@ function atribuindoValor () {
         else if (categoria == 'mercearia') {
             mercearia.push(item)
         }
-    }  
+    } 
+
+    if (adicionarItem == 'Não') {
+
+        alert('Lista finalizada')
+                    
+        var categoriaFrutas = document.createElement('p')
+        categoriaFrutas.innerHTML = 'Frutas: ' + frutas
+
+        var categoriaAçougue = document.createElement('p')
+        categoriaAçougue.innerHTML = 'Açougue: ' + açougue
+
+        var categoriaMercearia = document.createElement('p')
+        categoriaMercearia.innerHTML = 'Mercearia: ' + mercearia
+
+        lista.appendChild(categoriaFrutas)
+        lista.appendChild(categoriaAçougue)
+        lista.appendChild(categoriaMercearia)
+    }
 } 
+
+function removerValor () {
+    var remover = prompt('Qual item deseja retirar?')
+
+    if(frutas.indexOf(remover) != -1) {
+        frutas.splice(frutas.indexOf(remover), 1)
+        alert(`${remover} foi removido com sucesso`)
+    }
+    
+    else if (açougue.indexOf(remover) != -1) {
+        açougue.splice(açougue.indexOf(remover), 1)
+        alert(`${remover} foi removido com sucesso`)
+    }
+    
+    else if (mercearia.indexOf(remover) != -1) {
+        mercearia.splice(mercearia.indexOf(remover), 1)
+        alert(`${remover} foi removido com sucesso`)
+    }
+
+    alert('Lista finalizada')
+                    
+    var categoriaFrutas = document.createElement('p')
+    categoriaFrutas.innerHTML = 'Frutas: ' + frutas
+
+    var categoriaAçougue = document.createElement('p')
+    categoriaAçougue.innerHTML = 'Açougue: ' + açougue
+
+    var categoriaMercearia = document.createElement('p')
+    categoriaMercearia.innerHTML = 'Mercearia: ' + mercearia
+
+    lista.appendChild(categoriaFrutas)
+    lista.appendChild(categoriaAçougue)
+    lista.appendChild(categoriaMercearia)
+
+}
