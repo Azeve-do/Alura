aeaeb0c41178143fb20b7fb3137645235b8ad3b5
Azeve-do <caiopaznunes@gmail.com>
2023-08-10 20:53:43 -0300

    calculadora1 finalizada

--- excercises/7DAYS_OF_CODE/dia_07_Calculadora/js.js
+++ excercises/7DAYS_OF_CODE/dia_07_Calculadora/js.js
@@ -1,35 +1,41 @@
-const teclado = document.querySelectorAll('input[type=button]')
+const teclado = document.querySelectorAll('.operacao')
 const display = document.querySelector('input[type=number]')
+const limpar = document.querySelector('#clear')
+
+var valor1 = ''
+var operacaoSelecionada = ''
 
 for (let i = 0; i < teclado.length; i++) {
     const teclas = teclado[i]
     
     teclas.addEventListener('click', () => {
-    display.value = display.value + teclas.value
+        if(teclas.value === '+' || teclas.value === '-' || teclas.value === '*' || teclas.value === '/'){
+            operacaoSelecionada = teclas.value
+            valor1 = Number(display.value)
+            display.value = ''
+        } else if (teclas.value === '=') {
+            resultado()
+        } else if (teclas.value === 'C') {
+            display.value = ''
+        } else {
+            display.value = display.value + teclas.value
+        }
+
     })
 }
 
-function soma() {
-    var valor = display.value
-    console.log(valor)
+function resultado() {
+    var valor2 = Number(display.value)
+    
+    switch (operacaoSelecionada) {
+        case '+':
+            resultadoFinal = valor1 + valor2
+            break
+    }
 
+    display.value = resultadoFinal
 }
 
-function subtração () {
-
-}
-
-function divisão () {
-
-}
-
-function multiplicação () {
-
-}
-
-function resultado(valor) {
-    var valor2 = display.value
-
-    console.log(valor2)
-
-}
+limpar.addEventListener('click', () => {
+    display.value = ''
+})
