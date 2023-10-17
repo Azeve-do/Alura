b3ea909a6e779732ee9d4930276f35016ae9b6a9
Azeve-do <caiopaznunes@gmail.com>
2023-10-16 22:56:16 -0300

    algoritmos

---
+++ aula12__algoritmos/valorMenorLivros/precosLivros.js
@@ -0,0 +1,33 @@
+const precoLivros = [
+    {
+        titulo: "JavaScript",
+        preco: 25
+    },
+
+    {
+        titulo: "PHP",
+        preco: 15
+    },
+
+    {
+        titulo: "JAVA",
+        preco: 30
+    },
+
+    {
+        titulo: "ELIXIR",
+        preco: 50
+    },
+
+    {
+        titulo: "GO",
+        preco: 45
+    },
+
+    {
+        titulo: "PYTHON",
+        preco: 20
+    }
+]
+
+module.export = precoLivros;
