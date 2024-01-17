e181654ee331333d89a22b6fb30bcc11310e4a0b
Caio <122945848+Azeve-do@users.noreply.github.com>
2024-01-17 09:50:31 -0300

    jsscript

---
+++ excercises/testesmeu/ClassRoom/script.js
@@ -0,0 +1,8 @@
+var dezR = document.getElementById('10r')
+var cincoR = document.getElementById('5r')
+var tresR = document.getElementById('3r')
+var semLimites = document.getElementById('semL')
+
+dezR.addEventListener('click', ()=> {
+    
+})
