76d3b741fd08dfb8034374c50f87369965125bfa
Azeve-do <caiopaznunes@gmail.com>
2024-02-08 21:08:41 -0300

    Trocar janela OK

--- excercises/testesmeu/ClassRoom/script.js
+++ excercises/testesmeu/ClassRoom/script.js
@@ -1,11 +1,28 @@
-var dezR = document.getElementById('10r')
-var cincoR = document.getElementById('5r')
-var tresR = document.getElementById('3r')
-var semLimites = document.getElementById('semL')
+const dezR = document.getElementById('10r')
+const cincoR = document.getElementById('5r')
+const tresR = document.getElementById('3r')
+const semLimites = document.getElementById('semL')
+const questaoProf = document.getElementById('professor')
+
+var quantidadeDeRodadas = 0
+
+var listaProfessores = ["Teste1", "teste2", "teste3", "teste4", "teste5", "teste6", "teste7", "teste8", "teste9", "teste10"]
 
 
+function alterarTexto(element, novoTexto) {
+    element.innerText = novoTexto
+}
 
 
 dezR.addEventListener('click', ()=> {
+
+    window.location.href = "Asrodadas.html"
     
+    quantidadeDeRodadas = 10;
+
+    alterarTexto(questaoProf, 'teste')
+
+    /* while (quantidadeDeRodadas < 10) {
+        questaoProf
+    }  */
 })
