6db2c78018d64ebc2341b1013160f45a7f545767
Azeve-do <caiopaznunes@gmail.com>
2024-02-20 21:13:13 -0300

    lista professores ok

--- excercises/testesmeu/ClassRoom/script2.js
+++ excercises/testesmeu/ClassRoom/script2.js
@@ -1,6 +1,33 @@
 
 
-var listaProfessores = ["Teste1", "teste2", "teste3", "teste4", "teste5", "teste6", "teste7", "teste8", "teste9", "teste10"]
+var listaProfessores = [
+    {nome: 'Luciana Cassia', turma: '1º Ano A' & '1º Ano G'},
+    {nome: 'Jussara', turma: '1º Ano B'},
+    {nome: 'Aldenice', turma: '1º Ano C' & '2º Ano G'},
+    {nome: 'Marli', turma: '1º Ano D'},
+    {nome: 'Ana Marta', turma: '1º Ano E'},
+    {nome: 'Ivana', turma: '1º Ano F'},
+    {nome: 'Sineia', turma: '2º Ano A'},
+    {nome: 'Edjane', turma: '2º Ano B'},
+    {nome: 'Lenice', turma: '2º Ano C'},
+    {nome: 'Ana Cristina', turma: '2º Ano D'},
+    {nome: 'Silvia Araujo', turma: '2º Ano E'},
+    {nome: 'Graça', turma: '2º Ano F' & '3º Ano B'},
+    {nome: 'Rosimere', turma: '3º Ano A'},
+    {nome: 'Giscelia', turma: '3º Ano C'},
+    {nome: 'Adriana Matreiro', turma: '3º Ano D'},
+    {nome: 'Silvia Maria', turma: '3º Ano E'},
+    {nome: 'Valeria', turma: '3º Ano F'},
+    {nome: 'Adriana Dias', turma: '4º Ano A' & '5º Ano E'},
+    {nome: 'Vanilza', turma: '4º Ano B' & '5º Ano C'},
+    {nome: 'Lucimara Faria', turma: '4º Ano C'},
+    {nome: 'Vilma', turma: '4º Ano D'},
+    {nome: 'Luciane', turma: '4º Ano E'},
+    {nome: 'Andrea', turma: '5º Ano A'},
+    {nome: 'Jorge', turma: '5º Ano B'},
+    {nome: 'Adriane Kenia', turma: '5º Ano D'},
+    
+]
 
 
 const professorAleatorioIndex = Math.floor(Math.random() * listaProfessores.length);
