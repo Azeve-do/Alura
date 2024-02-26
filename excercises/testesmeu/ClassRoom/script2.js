63ecd6c35c9d871eb6b19c357e8c04cd09ffb397
Azeve-do <caiopaznunes@gmail.com>
2024-02-25 21:12:35 -0300

    remover duplicadas ok

--- excercises/testesmeu/ClassRoom/script2.js
+++ excercises/testesmeu/ClassRoom/script2.js
@@ -26,6 +26,41 @@ var listaProfessores = [
     {nome: 'Adriane Kenia', turma: '5º Ano D'},
     
 ]
+const listaSalas = [
+    "1º Ano A",
+    "1º Ano B",
+    "1º Ano C",
+    "1º Ano D",
+    "1º Ano E",
+    "1º Ano F",
+    "1º Ano G",
+    "2º Ano A",
+    "2º Ano B",
+    "2º Ano C",
+    "2º Ano D",
+    "2º Ano E",
+    "2º Ano F",
+    "2º Ano G",
+    "3º Ano A",
+    "3º Ano B",
+    "3º Ano C",
+    "3º Ano D",
+    "3º Ano E",
+    "3º Ano F",
+    "4º Ano A",
+    "4º Ano B",
+    "4º Ano C",
+    "4º Ano D",
+    "4º Ano E",
+    "5º Ano A",
+    "5º Ano B",
+    "5º Ano C",
+    "5º Ano D",
+    "5º Ano E",
+]
+
+var listaEscolhasAleatorias = []
+
 
 const alternativa = document.getElementById('alternativa1')
 const alternativaDois = document.getElementById('alternativa2')
@@ -33,35 +68,32 @@ const alternativaTres = document.getElementById('alternativa3')
 const alternativaQuatro = document.getElementById('alternativa4')
 
 const professorAleatorioIndex = Math.floor(Math.random() * listaProfessores.length);
-const turmaAleatorio = listaProfessores[professorAleatorioIndex].turma
 const professorAleatorio = listaProfessores[professorAleatorioIndex].nome
 
 questaoProf.innerText = professorAleatorio
-alternativa.innerText = turmaAleatorio
-alternativaDois.innerText = turmaAleatorio
-alternativaTres.innerText = turmaAleatorio
-alternativaQuatro.innerText = turmaAleatorio
 
 
-const listaSalas = ["1º Ano A", "1º Ano B", "1º Ano C", "1º Ano D", "1º Ano E", "1º Ano F", "1º Ano G", "2º Ano A", "2º Ano B", "2º Ano C", "2º Ano D", "2º Ano E", "2º Ano F", "2º Ano G", "3º Ano A", "3º Ano B", "3º Ano C", "3º Ano D", "3º Ano E", "3º Ano F", "4º Ano A", "4º Ano B", "4º Ano C", "4º Ano D", "4º Ano E", "5º Ano A", "5º Ano B","5º Ano C", "5º Ano D", "5º Ano E"]
+window.onload = function alternativasAleatorias () {        
+    i = 1
 
+    while (i <= 4) {
 
+        var opçoesIndex = Math.floor(Math.random () * listaSalas.length)
+        var opçoesAleatorio = listaSalas[opçoesIndex]
 
-/* var opçoesIndex = Math.floor(Math.random() * listaSalas.length);
-var opçoesIndex2 = Math.floor(Math.random() * listaSalas.length);
-var opçoesIndex3 = Math.floor(Math.random() * listaSalas.length);
-var opçoesIndex4 = Math.floor(Math.random() * listaSalas.length);
+        listaEscolhasAleatorias.push(opçoesAleatorio)
+        i ++
 
-var opçoesAleatorio = listaSalas[opçoesIndex]
-var opçoesAleatorio2 = listaSalas[opçoesIndex2]
-var opçoesAleatorio3 = listaSalas[opçoesIndex3]
-var opçoesAleatorio4 = listaSalas[opçoesIndex4]  */
+    } 
 
+    const alternativasUnicas = Array.from(new Set(listaEscolhasAleatorias))
 
+    while (alternativasUnicas.length < 4){
 
+        var opçoesIndex = Math.floor(Math.random () * listaSalas.length)
+        var opçoesAleatorio = listaSalas[opçoesIndex]
 
-
-function alternativasAleatorias () {
-    let contagem = 0
+        alternativasUnicas.push(opçoesAleatorio)
+    }
     
 }
