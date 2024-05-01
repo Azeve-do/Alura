ed245a3c4ae09c1230e2a827eb412b0ee207f14c
Azeve-do <caiopaznunes@gmail.com>
2024-04-30 21:41:29 -0300

    Update js.js

--- aula16_objetos_ORIE/js.js
+++ aula16_objetos_ORIE/js.js
@@ -2,12 +2,23 @@ const user = {
     nome: "Caio",
     DN: "07/05/2023",
     email: "caio@paz.com",
-    role: "admin",
+    role: "estudante",
     ativo: true,
     exibirInfos: function () {
         console.log(this.nome, this.email)
     }
 }
+
+const admin = {
+    nome: "Reginaldo",
+    email: "naldo@s.com",
+    role: "admin",
+    criaCurso() {
+        console.log('curso criado!')
+    }
+}
+
+Object.setPrototypeOf(admin, user)
 
 user.exibirInfos()
 
